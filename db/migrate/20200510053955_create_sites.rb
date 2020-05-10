528a43b07aa14d6f26f32d7fb165f63c061ac636
Jon Norton <jmichaeln5@gmail.com>
2020-05-10 01:40:29 -0400

    Added sites controller

---
+++ db/migrate/20200510053955_create_sites.rb
@@ -0,0 +1,11 @@
+class CreateSites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sites do |t|
+      t.string :link
+      t.string :title
+      t.string :keyword
+
+      t.timestamps
+    end
+  end
+end
